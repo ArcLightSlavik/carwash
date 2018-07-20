c091e7fa761395e3e48de070c165567927047663
Cartoonz_S <slavko.leskiv@gmail.com>
2018-07-20 07:22:43 +0300

    Validations - github broke so doesn't show new files/changed stuff

--- src/main/java/ua/slavik/carwash/exception/validators/Description.java
+++ src/main/java/ua/slavik/carwash/exception/validators/Description.java
@@ -8,7 +8,8 @@ import java.lang.annotation.*;
 @Constraint(validatedBy = PhoneNumberValidator.class)
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Description {
+public @interface Description
+{
     String message() default "{Description}";
 
     Class<?>[] groups() default {};
